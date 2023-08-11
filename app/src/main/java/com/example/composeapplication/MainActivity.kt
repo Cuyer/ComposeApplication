cd88471ff9362db210e0009a375385afb6aabaa3
Krystian Checinski <kchecinski@doz.pl>
2023-08-11 10:05:41 +0200

    Navigation setup

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -7,7 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.composeapplication.navigation.navigate
 import com.example.composeapplication.ui.theme.ComposeApplicationTheme
+import com.example.core.navigation.Route
 import com.example.onboarding_presentation.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
@@ -15,12 +20,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeApplicationTheme {
-                // A surface container using the 'background' color from the theme
+                val navController = rememberNavController()
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WelcomeScreen()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME
+                    ) {
+                        composable(Route.WELCOME) {
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.AGE) {
+
+                        }
+                        composable(Route.GENDER) {
+
+                        }
+                        composable(Route.HEIGHT) {
+
+                        }
+                        composable(Route.WEIGHT) {
+
+                        }
+                        composable(Route.NUTRIENT_GOAL) {
+
+                        }
+                        composable(Route.ACTIVITY) {
+
+                        }
+                        composable(Route.GOAL) {
+
+                        }
+                        composable(Route.TRACKER_OVERVIEW) {
+
+                        }
+                        composable(Route.SEARCH) {
+
+                        }
+                    }
                 }
             }
         }
